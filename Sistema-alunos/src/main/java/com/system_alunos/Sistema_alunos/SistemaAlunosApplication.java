ab567e6c35086dd0c1640bd938d077f08f7ff90b
Natan <natanhenriqueramossilva@gmail.com>
2026-02-02 19:50:35 -0300

    Initial commit

---
+++ Sistema-alunos/src/main/java/com/system_alunos/Sistema_alunos/SistemaAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.system_alunos.Sistema_alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAlunosApplication.class, args);
+	}
+
+}
---
+++ Sistema-alunos/src/test/java/com/system_alunos/Sistema_alunos/SistemaAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system_alunos.Sistema_alunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
